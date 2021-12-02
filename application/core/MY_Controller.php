1bc84faca6f6a3cddf0ed023f9ea12f69173608b
Muhammad Suryono <msuryono0@gmail.com>
2021-12-02 15:58:37 +0700

    Add function helper in MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -42,4 +42,92 @@ class MY_Controller extends CI_Controller {
 			"data" => $data 
 		];
 	}
+
+	private function upload_files($path, $title, $files)
+    {
+        $config = array(
+            'upload_path'   => $path,
+            'overwrite'     => 1,                       
+        );
+
+        $this->load->library('upload', $config);
+
+        $images = array();
+
+        foreach ($files['name'] as $key => $image) {
+            $_FILES['images[]']['name']= $files['name'][$key];
+            $_FILES['images[]']['type']= $files['type'][$key];
+            $_FILES['images[]']['tmp_name']= $files['tmp_name'][$key];
+            $_FILES['images[]']['error']= $files['error'][$key];
+            $_FILES['images[]']['size']= $files['size'][$key];
+
+            $fileName = $title .'_'. $image;
+
+            $images[] = $fileName;
+
+            $config['file_name'] = $fileName;
+
+            $this->upload->initialize($config);
+
+            if ($this->upload->do_upload('images[]')) {
+                $this->upload->data();
+            } else {
+                return false;
+            }
+        }
+
+        return $images;
+    }
+
+	public function parseDateToTimestampApi($dateString) 
+	{
+		$data = "";
+		$d = DateTime::createFromFormat('Y-m-d H:i:s', $dateString.' 00:00:00');
+		if ($d === false) {
+			$data = "Incorect Date";
+		} else {
+			$data = $d->getTimestamp();
+		}
+
+		return $data;
+	}
+
+	public function getNumberOfWeek($date) 
+	{
+		$date = new DateTime($date);
+		$week = $date->format("W");
+		return $week;
+	}
+
+	public function getDayNameOfDate($date)
+	{
+		$datetime = DateTime::createFromFormat('Y-m-d', $date);
+    	return $datetime->format('l');
+	}
+
+	public function dateNowTimeStamp()
+	{
+		$now = new DateTime();
+		$now->format('Y-m-d H:i:s'); 
+		return $now->getTimestamp();
+	}
+
+	public function getStartAndEndDate($week) 
+	{
+		$year = date("Y");
+		$dto = new DateTime();
+		$dto->setISODate($year, $week);
+		$ret['week_start'] = $dto->format('Y-m-d');
+		$dto->modify('+5 days');
+		$ret['week_end'] = $dto->format('Y-m-d');
+		return $ret;
+	}
+
+	public function selisihHariByDate($date1, $date2)
+	{
+		$date1 = new DateTime($date1); 
+		$date2 = new DateTime();
+		$compare = $date1->diff($date2);
+		return $compare;
+	}
 }
