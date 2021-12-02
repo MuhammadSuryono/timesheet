424a579accdf36d566ba89287735cbcaafbe03a5
Muhammad Suryono <msuryono0@gmail.com>
2021-12-02 16:01:38 +0700

    Adjustment page discuss

--- application/views/discuss/index.php
+++ application/views/discuss/index.php
@@ -11,6 +11,7 @@
 	<div class="section-body">
 		<div class="row">
 			<div class="col-lg-12">
+				<div id="alert-notification"></div>
 				<div class="card">
 					<div class="card-header d-flex justify-content-between align-items-center">
 						<h2 class="section-title title-information">Informasi Pekerjaan</h2>
@@ -34,12 +35,22 @@
 										<td>&nbsp;<?= ucwords($titleDeskripsiPekerjaan) ?></td>
 									</tr>
 									<tr>
-										<td>Tanggal Mulai</td>
+										<td>Tanggal Input Pekerjaan</td>
 										<td>:</td>
-										<td>&nbsp;<?= isset($dataInformationTask['daritanggal']) ? $dataInformationTask['daritanggal'] : '-' ?></td>
+										<td>&nbsp;<?= isset($dataInformationTask['tanggalisi']) ? $dataInformationTask['tanggalisi'] : '-' ?></td>
 									</tr>
 									<tr>
 										<td>Tanggal Selesai</td>
+										<td>:</td>
+										<td>&nbsp;<?= isset($dataInformationTask['tanggal_input_selesai']) ? $dataInformationTask['tanggal_input_selesai'] : '-' ?></td>
+									</tr>
+									<tr>
+										<td>Tanggal Target Selesai</td>
+										<td>:</td>
+										<td>&nbsp;<?= isset($dataInformationTask['tanggal_target_seelsai_rincian']) ? $dataInformationTask['tanggal_target_seelsai_rincian'] : '-' ?></td>
+									</tr>
+									<tr>
+										<td>Tanggal Deadline Mingguan</td>
 										<td>:</td>
 										<td>&nbsp;<?= isset($dataInformationTask['sampaitanggal']) ? $dataInformationTask['sampaitanggal'] : '-' ?></td>
 									</tr>
@@ -83,6 +94,11 @@
 										<td>&nbsp;<?= isset($dataInformationTask['status_perpanjang']) && $dataInformationTask['status_perpanjang'] != "" ? $dataInformationTask['status_perpanjang'] : 'Tidak Ada' ?> <i class="fa fa-question-circle pointer"></i></td>
 									</tr>
 									<tr>
+										<td>Pencapaian</td>
+										<td>:</td>
+										<td>&nbsp;<?= isset($dataInformationTask['status_perpanjang']) && $dataInformationTask['status_perpanjang'] != "" ? $dataInformationTask['status_perpanjang'] : 'Tidak Ada' ?> <i class="fa fa-question-circle pointer"></i></td>
+									</tr>
+									<tr>
 										<td>Status Pekerjaan</td>
 										<td>:</td>
 										<?php
@@ -102,7 +118,7 @@
 									<tr>
 										<td>Persentase Hasil pekerjaan</td>
 										<td>:</td>
-										<td>&nbsp;<i class="fa fa-chart-bar" aria-hidden="true"></i> <?= $statusPekerjaan = isset($dataInformationTask['persentase'])  ? $dataInformationTask['persentase'] : '0'?>%</td>
+										<td>&nbsp;<i class="fa fa-chart-bar" aria-hidden="true"></i> <?= isset($dataInformationTask['persentase'])  ? $dataInformationTask['persentase'] : '0'?>%</td>
 									</tr>
 								</table>
 							</div>
@@ -115,7 +131,9 @@
 			<div class="col-lg-12">
 				<div class="card">
 					<div class="card-header">
-						<button class="btn btn-primary" id="btn-create-discuss"><i class="fa fa-plus"></i> Tambah Diskusi</button>
+						<?php if ($statusPekerjaan != "Selesai") {
+							echo '<button class="btn btn-primary" id="btn-create-discuss"><i class="fa fa-plus"></i> Tambah Diskusi</button>';
+						} ?>
 					</div>
 					<div class="card-body">
 						<div class="row">
@@ -153,7 +171,7 @@
 				<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
-			<form method="POST" enctype="multipart/form-data">
+			<form enctype="multipart/form-data">
 			<div class="modal-body">
 				<div id="alert-form"></div>
 				<div class="form-group">
@@ -188,10 +206,34 @@
 	</div>
 </div>
 
+<div class="modal fade" id="loading-prosess" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="item"><i class="loader --1"></i></div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="detail-discuss" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Isi Diskusi</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div id="content-discuss"><div class="item"><i class="loader --1"></i></div></div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <script>
 
 	let stateForm = "create"
-
+	let stateIdDiscuss = 0;
 	$(document).ready(function() {
 		const btnCreateDiscuss = $('#btn-create-discuss')
 		const btnSubmitCreateDiscuss = $('#submitCreateDiscuss');
@@ -226,7 +268,7 @@
 
 			if (data.length > 0) {
 				data.forEach((element, index) => {
-					htmlTableDataDiscuss += `<tr><td class='text-center'>${index + 1}</td><td>${element.title}</td><td>${element.mentor}</td><td class="text-center">${element.created_at}</td><td class="text-center">${element.updated_at}</td><td><button class="btn btn-primary btn-sm mr-1"><i class="fa fa-eye"></i>&nbsp;Detail</button><button class="btn btn-success btn-sm mr-1"><i class="fa fa-edit"></i>&nbsp;Edit</button><button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i>&nbsp;Hapus</button></td></tr>`
+					htmlTableDataDiscuss += `<tr><td class='text-center'>${index + 1}</td><td>${element.title}</td><td>${element.mentor}</td><td class="text-center">${element.created_at}</td><td class="text-center">${element.updated_at}</td><td><button onclick="getDetailDiscuss(${element.id})" class="btn btn-primary btn-sm mr-1"><i class="fa fa-eye"></i>&nbsp;Detail</button><button onclick="getDataDiscussUpdate(${element.id})" class="btn btn-success btn-sm mr-1"><i class="fa fa-edit"></i>&nbsp;Edit</button><button onclick="hapus(${element.id}, ${taskId})" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i>&nbsp;Hapus</button></td></tr>`
 				});	
 				setTimeout(() => {
 					bodyTable.innerHTML = htmlTableDataDiscuss;	
@@ -237,6 +279,11 @@
 				}, delay)
 			}
 				
+		}).catch((e) => {
+			console.warn(e);
+			setTimeout(() => {
+				bodyTable.innerHTML = noDataTable();
+			}, delay)
 		});
 
 	}
@@ -261,9 +308,9 @@
 			.then(data => data);
 	}
 
-	function httpRequestPost(url, data) {
+	function httpRequest(url, data, method = "POST") {
 		return fetch(url, {
-			method: 'POST',
+			method: method,
 			body: data
 		}).then(res => res.json()).then(data => data)
 	}
@@ -290,7 +337,7 @@
 
 		previousPage = previousPage + "?lastData=true"
 
-		if (searchData !== undefined && searchData !== "") {
+		if (searchData !== undefined && searchData !== null && searchData !== "") {
 			let dataSearch = JSON.parse(searchData)
 
 			let queryUrl = `&isSearch${dataSearch.is_search}&idUser=${dataSearch.id_user}&startDate=${dataSearch.daritanggal}&endDate=${dataSearch.sampaitanggal}&keyword=${dataSearch.katakunci}`
@@ -300,7 +347,7 @@
 		window.location.href = previousPage
 	}
 
-	function getUserManager() {
+	function getUserManager(idMentor = 0) {
 		const optionMentors = document.getElementById("list-mentor")
 		httpRequestGet('/api/user/manager').then((res) => {
 			let data = res.data
@@ -308,7 +355,8 @@
 
 			data.forEach((elm,i) => {
 				let selected = ""
-				if(elm.is_leader) selected = "selected";
+				if(elm.is_leader && idMentor === 0) selected = "selected";
+				if(idMentor !== 0 && idMentor === elm.id) selected = "selected";
 				options += `<option value='${elm.id}' ${selected}>${elm.name}</option>`
 			})
 
@@ -329,51 +377,111 @@
 		formData.append('mentor_discuss', mentorDiscuss)
 		formData.append('result_discuss', resultDiscuss)
 
-		httpRequestPost('/api/discuss/create', formData).then((res) => {
+		let pathSubmit = '/api/discuss/create'
+		let method = "POST"
+		if (stateForm === "update") {
+			pathSubmit = '/api/discuss/update/' + stateIdDiscuss;
+			method = "PUT"
+		}
+
+		httpRequest(pathSubmit, formData, method).then((res) => {
 			elmButtonSubmit.html('Simpan')
 			if (res.is_success) {
 				notifAlertForm.innerHTML = alertForm("success", res.message);
 				setDataTableDiscuss(taskId)
 				setTimeout(() => {
+					notifAlertForm.innerHTML = "";
 					hideModal('createDiscuss')
 				}, 1000)
 			} else {
+				setTimeout(() => {
+					notifAlertForm.innerHTML = "";
+				}, 5000)
 				notifAlertForm.innerHTML = alertForm("danger", res.message);
 			}
 		}).catch((e) => {
 			console.warn(e)
+			setTimeout(() => {
+				notifAlertForm.innerHTML = "";
+			}, 5000)
 			notifAlertForm.innerHTML = alertForm("danger", "Terjadi masalah ketika membuat diskusi");
 		})
 	}
 
 	function setPointTask(taskId) {
+		const pointElement = document.getElementById('point-task').innerHTML
 		httpRequestGet('/api/discuss/point/task/' + taskId).then((res) => {
-			console.log(res)
-			let data = res.data
-			let point = 100
-			let persentase = parseInt(data.persentase)
+			$('#point-task').html(res.data)
+		})
+	}
 
-			let startDate = Date.parse(data.daritanggal)
-			let endDate = Date.parse(data.sampaitanggal)
-			let lastUpdateDate = Date.parse(data.tanggal_input_selesai)
-			let dateTarget = Date.parse(data.tanggal_target_seelsai_rincian)
+	function hapus(id, taskId) {
+		const notifAlertForm = document.getElementById('alert-notification')
+		alertSwal().then((result) => {
+			if (result.value) {
+				showModal("loading-prosess")
+				setTimeout(() => {
+					hideModal("loading-prosess")
+				}, 5000)
+				httpRequest('/api/discuss/delete/task/' +id, [], "DELETE").then((res) => {
+					if (res.is_success) {
+						setDataTableDiscuss(taskId)
+						setTimeout(() => {
+							notifAlertForm.innerHTML = ""
+						}, 2000)
+						notifAlertForm.innerHTML = alertForm("success", res.message)
+					} else {
+						setTimeout(() => {
+							notifAlertForm.innerHTML = ""
+						}, 2000)
+						notifAlertForm.innerHTML = alertForm("danger", res.message)
+					}
 
-			if (lastUpdateDate < endDate) {
-				if (persentase < 100 && lastUpdateDate < dateTarget) {
-					point = persentase
-				} else if (persentase >= 100 && lastUpdateDate < dateTarget) {
-					point = 100
-				}	
-			} else if (lastUpdateDate > endDate) {
-				point = 0
-				if (persentase < 100 && dateTarget > endDate && lastUpdateDate < dateTarget) {
-					point = persentase - 25 <= 0 ? 0 : persentase - 25
-				} else if (persentase >= 100 && dateTarget > endDate && lastUpdateDate < dateTarget) {
-					point = point - 25 <= 0 ? 0 : point - 25
-				}
+				})
 			}
+		}).catch((e) => {
+			console.warn(e)
+			notifAlertForm.innerHTML = alertForm("danger", "Terjadi masalah ketika membuat diskusi");
+			setTimeout(() => {
+				notifAlertForm.innerHTML = ""
+			}, 2000)
+		})
+	}
 
-			document.getElementById('point-task').innerHTML = '<div>100</div>'
+	function alertSwal() {
+		return Swal({
+			title: 'Apakah anda yakin ?',
+			text: "data akan dihapus",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Hapus Data!'
+		})
+	}
+
+	function getDetailDiscuss(id) {
+		const content = document.getElementById("content-discuss")
+		showModal("detail-discuss")
+		httpRequestGet('/api/discuss/task/' + id).then((res) => {
+			let data = res.data
+			content.innerHTML = `<p>${data.discussion_results}`;
+
+		})
+	}
+
+	function getDataDiscussUpdate(id) {
+		showModal('createDiscuss')
+		stateForm = "update"
+		stateIdDiscuss = id
+
+		httpRequestGet('/api/discuss/task/' + id).then((res) => {
+			let data = res.data
+			if (res.is_success) {
+				document.getElementById("title-discuss").value = data.title;
+				document.getElementById("results-discuss").value = data.discussion_results;
+				getUserManager(data.id_user_mentor)
+			}
 		})
 	}
 
